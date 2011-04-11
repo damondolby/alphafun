ddb8d8a97e70e95e7607e65060a7d655580e0354
damondolby <damondolby@gmail.com>
2011-04-11 21:51:24 +0100

    =Introduction of Games 1-5

--- alphafun.js
+++ alphafun.js
@@ -14,17 +14,21 @@ var color, hue = [
 
 
 
-var x;
-var y;
-var t;
+//var x;
+//var y;
+//var t;
 //var correct = 0;
 //var goes = 0;
-var limit = 5;
-var run = 0;
-var lives;
-var score_incr = 10;
-var score = 0;
-var speed;
+//var limit = 5;
+//var run = 0;
+//var lives;
+//var score_incr = 10;
+var totalscore = 0;
+//var speed;
+var game;
+var levelno = 1;
+var maxgoes = 10;
+var gametotal = 5;
 
 //var colours = ["orange", "red", "blue", "green", "yellow"];
 //var current_colour;
@@ -34,185 +38,231 @@ var speed;
 
   // The hue spectrum used by HSV color picker charts.
   
-var c_idx;
-  
-//function Level(letters){
-//	this.letters = letters;
-//	
+//var c_idx;
+
+
+function Game(level) {
+	this.lives = 3;
+	this.missed = 0;
+	this.speed = 20;
+	this.score = 0;
+	this.active = 0;
+	this.level = level;
+	this.score_incr = 10;
+	this.timeout;
+	this.x = 0;
+	this.y = 75;
+	this.font = 'bold 120px sans-serif';
+	this.go = 0;	
 	
+	this.resetxy = function()  {
+		this.x = 0;
+		this.y = 75;
+	}	
+	
+	this.incrementxy = function() {
+		this.x += 2;
+		this.y += 2;
+	}
+	
+	this.increasespeed = function () {
+		if (this.speed >= 1)
+			this.speed--;
+	}
+	
+	this.gotcorrect = function () {
+		this.score += this.score_incr;
+		this.go++;
+		this.updatescore();	
 		
-//}
-
-
-function init(){
-	//resetxy();
-	
-	//c_idx = Math.floor(Math.random()*27)-1;
-	
-	$(document).keydown(listen);
-	
-	canvasStart();
-	
-	updateScore();
-	
-	//play();	
-}
-
-function listen(event){			
-	
-	if (run == 0)
-	{
-		if (event.keyCode == 13)
-		{
-			//game end
-			startGame();
+		if (this.go >= maxgoes)
+			this.end();
+		else
+		{			
+			this.increasespeed();
+			this.level.updateletter();
+			this.resetxy();
 		}
 	}
-	else
-	{
-		//mid-game
-		if (event.keyCode == cs[c_idx])
-		{
-			//alert('You pressed '+event.keyCode);
-			score += score_incr;
-			
-			increaseSpeed();
 	
-			updateLetter();
+	this.keypressed = function (keycode) {
+		if(this.level.keypressed(keycode))
+			this.gotcorrect();
+	}
+	
+	this.gotwrong = function () {
+		//this.lives--;
+		this.missed++;
+		this.go++;
+		
+		if (this.go >= maxgoes)
+			this.end();
+		else
+		{
+		
+			this.updatescore();	
+		//if (this.lives <= 0) {
+		//	this.end();
+		//}
+		//else {
+			this.level.updateletter();
+			this.resetxy();
+		//}			
 		}
+	}
+	
+	this.updatescore = function () {
+		document.getElementById("gamemissed").innerHTML = this.missed;
+		document.getElementById("gamescore").innerHTML = this.score;
+	}
+	
+	this.start = function () {
+		this.active = 1;
+		this.updatescore();
+		this.level.updateletter();
+		this.play();
+	}
+	
+	this.end = function () {
+		totalscore += this.score;
+		clearTimeout(this.timeout);
+		this.active = 0;
+		document.getElementById("letter2").innerHTML = "end";
+		canvastext();			
+	}
+}
+
+function play() {
+	
+	if (this.active == 0)
+		return;
+
+	//with (this) {
+		//document.getElementById("dt").innerHTML = new Date();
+		var canvas = document.getElementById('canvas');
+		if (canvas.getContext){
+			var ctx = canvas.getContext('2d');
+			ctx.clearRect(0,0,width,height); // clear canvas
+			ctx.beginPath();	
+			
+			///ctx.font  = this.font;			
+			ctx.font = 'bold 60px sans-serif';
+			//ctx.font-size = 120px;
+			//ctx.font-weight = bold;
+			var gradient = ctx.createLinearGradient(0, 0, width, height);
+			
+			  // Add the color stops.
+			  for (var i = 0; i <= 6; i++) {
+			    color = 'rgb(' + hue[i][0] + ', ' + hue[i][1] + ', ' + hue[i][2] + ')';
+			    gradient.addColorStop(i * 1/27, color);
+				//gradient.addColorStop(i * 1/13, color);
+			    gradient.addColorStop((i+7) * 1/27, color);
+				  gradient.addColorStop((i+14) * 1/27, color);
+				  gradient.addColorStop((i+21) * 1/27, color);
+			  }		
+			  			
+			// Assign our gradient to the fillStyle
+			ctx.fillStyle = gradient;
+			  
+			  
+			  
+			//ctx.fillText(ls[this.level.letters[0]], this.x, this.y);
+			  ctx.fillText(this.level.concatenate(), this.x, this.y);
+			ctx.closePath();                                
+			ctx.stroke();
+			
+			if (this.x >= width-20) {
+				this.gotwrong();
+			}
+			else {
+				this.incrementxy();
+			}
+			
+			
+		}
+		
+		if (this.active == 1) {
+				//use closure to recursively call play so it can reference Game fields
+				var gthis = this
+				timeout=setTimeout(function(){ gthis.play();}, this.speed);
+		}
+	//}
+}
+Game.prototype.play = play;
+
+
+function init() {
+	$(document).keydown(listen);	
+	canvastext();
+	document.getElementById("gametotal").innerHTML = gametotal;
+	//updateScore();
+}
+
+function listen(event) {			
+	
+	if (game == null || game.active == 0) {		
+		if (levelno <= gametotal)	{
+			if (event.keyCode == 13) {
+				document.getElementById("totalscore").innerHTML = totalscore;
+				document.getElementById("gamecurr").innerHTML = levelno;
+				game = new Game(new Level(levelno++));	
+				game.start();
+			}
+		}
+	}
+	else	{
+		game.keypressed(event.keyCode);
 	}
 	event.preventDefault();			
 }
 
-function increaseSpeed()
-{
-	/*if (speed >= 8)
-	{
-		speed--;
-		if (speed >= 20)
-			speed--;
-	}*/
-	if (speed >= 1)
-		speed--;
-}
 
-function updateScore()
-{
-	document.getElementById("my_lives").innerHTML = lives;
-	document.getElementById("my_score").innerHTML = score;
-}
-
-function resetxy()
-{
-	x = 0;
-	y = 75;
-}		
-
-function updateLetter()
-{
-	//goes++;
+function Level(noofletters) {
+	this.letters = new Array(noofletters);
+	this.letterscorrect = 0;
 	
-	//document.getElementById("speed").innerHTML = speed;
-	updateScore();
-	
-	if (lives <= 0)
-	{
-		clearTimeout(t);
-		run = 0;
-		canvasEnd();
-		return;
+	this.updateletter = function () {
+		//document.getElementById("letter2").innerHTML = this.letters.length;
+		//this.letters[0] = getRandonNumber(26);
+		for (i=0; i<this.letters.length; i++)
+			this.letters[i] = getRandonNumber(26);
+		
+		this.letterscorrect = 0;		
 	}
-	c_idx = Math.floor(Math.random()*26);
-	//current_colour = colours[Math.floor(Math.random()*5)];
-		// Add 2 steps to it
-	//rgb1 = "rgb("+getRandonNumber(155)+","+getRandonNumber(155)+","+getRandonNumber(155)+")";
-	//rgb2 = "rgb("+getRandonNumber(155)+","+getRandonNumber(155)+","+getRandonNumber(155)+")";
-	//element = document.getElementById("idx").innerHTML = c_idx;
-	resetxy();
-	//play();	
+	
+	this.concatenate = function() {
+		//return ls[this.letters[0]];
+		var r = "";
+		for (i=0; i<this.letters.length; i++)
+			r += ls[this.letters[i]];
+		//document.getElementById("letter2").innerHTML = r;
+		return r;
+	}
+	
+	this.keypressed = function(keycode) {
+		if (keycode == cs[this.letters[this.letterscorrect]])
+			this.letterscorrect++;
+		
+		return this.letters.length == this.letterscorrect;
+		//if (this.letters.length == this.letterscorrect)
+		//	return true;
+		//else
+		//	return false;
+	}
 }
 
-function getRandonNumber(upper)
-{
+
+function getRandonNumber(upper) {
 	return Math.floor(Math.random()*upper);
 }
 
-function startGame()
-{	
-	//goes = -1;
-	//correct = 0;
-	lives = 3;
-	score = 0;
-	speed = 20;
-	run = 1;
-	updateLetter();
-	play();	
-}
+/*function startGame() {	
+	level = new Level(1);
+	game = new Game(level);	
+	game.start();
+}*/
 
-function play(){
-
-	//document.getElementById("dt").innerHTML = new Date();
-	var canvas = document.getElementById('canvas');
-	if (canvas.getContext){
-		
-		var ctx = canvas.getContext('2d');
-		ctx.clearRect(0,0,width,height); // clear canvas
-
-		ctx.beginPath();	
-		
-		ctx.font  = 'bold 120px sans-serif';
-		//ctx.fillStyle = current_colour;
-		//ctx.strokeStyle = "black";
-		
-		var gradient = ctx.createLinearGradient(0, 0, width, height);
-		// Add 2 steps to it
-		//gradient.addColorStop(0, rgb1);
-		//gradient.addColorStop(1, rgb2);
-		
-		  // Add the color stops.
-		  for (var i = 0; i <= 6; i++) {
-		    color = 'rgb(' + hue[i][0] + ', ' + hue[i][1] + ', ' + hue[i][2] + ')';
-		    gradient.addColorStop(i * 1/27, color);
-			//gradient.addColorStop(i * 1/13, color);
-		    gradient.addColorStop((i+7) * 1/27, color);
-			  gradient.addColorStop((i+14) * 1/27, color);
-			  gradient.addColorStop((i+21) * 1/27, color);
-		  }		
-		  
-/*		  for (var i = 0; i <= 6; i++) {
-		    color = 'rgb(' + hue[i][0] + ', ' + hue[i][1] + ', ' + hue[i][2] + ')';
-		    gradient.addColorStop((i+7) * 1/13, color);
-		  }		*/
-		
-		// Assign our gradient to the fillStyle
-		ctx.fillStyle = gradient;
-
-		ctx.fillText(ls[c_idx], x, y);
-	
-		//ctx.strokeStyle = "orange";
-		
-		
-		ctx.closePath();
-		ctx.stroke();
-		
-		
-		if (x >= width-20)
-		{
-			lives--;
-			updateLetter();
-		}
-		else
-		{
-			x = x + 2;
-			y = y + 2;
-		}
-	}
-	
-	if (run == 1)
-		t=setTimeout("play()", speed);
-}
-
-function canvasStart(){
+function canvastext() {
 	var canvas = document.getElementById('canvas');
 	if (canvas.getContext){
 		
@@ -220,34 +270,14 @@ function canvasStart(){
 			
 		
 		ctx.clearRect(0,0,width,height); // clear canvas
-		
-		
 
 		ctx.beginPath();	
 				
-		ctx.font  = 'bold 40px sans-serif';
+		ctx.font  = 'bold 20px sans-serif';
 		ctx.fillStyle = "orange";
-		ctx.fillText("Press return key", 50, 125);
-		ctx.fillText("to start...", 90, 175);
-	}
-}
-
-function canvasEnd(){
-	var canvas = document.getElementById('canvas');
-	if (canvas.getContext){
-		
-		var ctx = canvas.getContext('2d');
-		ctx.clearRect(0,0,width,height); // clear canvas
-
-		ctx.beginPath();	
-		
-		ctx.font  = 'bold 40px sans-serif';
-		
-		ctx.fillStyle = "red";
-		ctx.fillText("Score " + score + "!", 50, 125);
-		
-		ctx.fillStyle = "orange";
-		ctx.fillText("Press return key", 50, 225);
-		ctx.fillText("to start...", 90, 275);
+		if (levelno > gametotal)
+			ctx.fillText("Thank you for playing.", 5, 125);
+		else			
+			ctx.fillText("Press return for game " + levelno, 5, 125);
 	}
 }
